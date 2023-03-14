1a6dab26892cde47e4ee8808686242a5b5dd8f1d
Steven <shnauzerr@hotmail.com>
2023-03-13 19:30:46 -0600

    Version 1.0.0

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
-import wget
+import requests
+import json
 
 TOKEN = "MTA4NDY1NTAzNjk2NzE2MjAwNg.GYi6TN.Xi8aGzvvIn8lnys5L96zIYBKLXnIldh_X4zSY4"
 
@@ -8,7 +9,19 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 def get_apex_map():
-    pass
+    query_params = {'version': 1}
+
+    url = 'https://api.mozambiquehe.re/maprotation?auth=ef056220ecc9350dd1214b2213643c87'
+    response = requests.get(url, params=query_params)
+
+    if response.status_code == 200:
+        data = response.json()
+
+    current_map = data['battle_royale']['current']['map']
+    time_remaining = data['battle_royale']['current']['remainingMins']
+    next_map = data['battle_royale']['next']['map']
+
+    return [current_map, time_remaining, next_map]
 
 @client.event
 async def on_ready():
@@ -30,7 +43,11 @@ async def on_message(message):
     
     if user_message.lower() == '!apexmap':
         info = get_apex_map()
-        await message.channel.send(info)
+        current_map = info[0]
+        time_remaining = info[1]
+        next_map = info[2]
+        await message.channel.send(f'The current map is {current_map} with {time_remaining} minutes remaining.')
+        await message.channel.send(f'Next map: {next_map}.')
         return
 
 
